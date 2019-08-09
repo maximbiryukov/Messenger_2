eefeeac4b13f52d0acbfe9756b28d2643583f89d
maximbiryukov <biryukov.maxim@gmail.com>
2019-08-09 17:04:40 +0300

    threading branch commit

--- server/__main__.py
+++ server/__main__.py
@@ -3,10 +3,27 @@ import json
 import time
 import logging
 import select
+import threading
 
 from handlers import handle_default_request
 
 from argparse import ArgumentParser
+
+
+def read(socket,connections, requests, buffersize):
+    try:
+        bytes_request = socket.recv(buffersize)
+    except Exception:
+        connections.remove(socket)
+    else:
+        requests.append(bytes_request)
+
+
+def write(socket, connections, response):
+    try:
+        write_client.send(response)
+    except Exception:
+        connections.remove(socket)
 
 
 parser = ArgumentParser()
@@ -59,18 +76,29 @@ try:
         )
 
         for read_client in rlist:
-            bytes_request = read_client.recv(config.get('buffersize'))
-            requests.append(bytes_request)
+            read_thread = threading.Thread(
+                target=read,
+                args=[read_client, connections, requests, config.get('buffersize')]
+            )
+            read_thread.start()
 
         if requests:
             bytes_request = requests.pop()
             bytes_response = handle_default_request(bytes_request)
 
             for write_client in wlist:
-                write_client.send(bytes_response)
+                write_thread = threading.Thread(
+                    target=write,
+                    args=[write_client, connections, bytes_response]
+                )
+                write_thread.start()
 
             for read_client in rlist:
-                read_client.send(bytes_response)
+
+                try:
+                    read_client.send(bytes_response)
+                except Exception:
+                    connections.remove(read_client)
 
 
 except KeyboardInterrupt:
